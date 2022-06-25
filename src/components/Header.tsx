66878273937dcb5285ed89a0d0b13e6615c116ea
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2022-06-25 13:38:33 -0300

    consertando erro header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
-
 import { Hamburger } from 'phosphor-react';
 import { Logo } from './Logo';
 
 interface Props {
-  isOpen: Boolean,
-  setIsOpen: Function,
+  isOpen: Boolean;
+  setIsOpen: Function;
 }
 
 export function Header(props: Props) {
-  
   return (
     <header className="w-full py-5 px-6 flex items-center justify-between lg:justify-center bg-gray-700 border-b border-gray-600 h-16 lg:h-auto">
       <Logo />
-      <h1 class="block lg:hidden" onClick={()=> props.setIsOpen(!props.isOpen)}>Aulas
+      <h1
+        className="block lg:hidden"
+        onClick={() => props.setIsOpen(!props.isOpen)}
+      >
+        Aulas
       </h1>
     </header>
   );
